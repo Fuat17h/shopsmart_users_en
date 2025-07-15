259997b7c8ee25ce481747d2635b10f4f595d98d
Fuat17h <Fuat17h@gmail.com>
2025-07-15 09:46:31 +0300

    initial commit ve dark and light mode using provider state management

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello World",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Hello World")),
+            SwitchListTile(
+              title: Text(
+                themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode",
+              ),
+              value: themeProvider.getIsDarkTheme,
+              onChanged: (value) {
+                themeProvider.setDarkTheme(themeValue: value);
+                log("Theme state ${themeProvider.getIsDarkTheme}");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
