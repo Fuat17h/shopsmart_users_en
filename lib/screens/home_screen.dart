797eed36e70a36efa1e34e34ea9407893cde5a56
Fuat17h <Fuat17h@gmail.com>
2025-07-16 12:22:36 +0300

    swiper in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,36 +1,53 @@
 // import 'dart:developer';
 
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shopsmart_users_en/widgets/subtitle_text.dart';
+import 'package:shopsmart_users_en/constants/app_constants.dart';
+import 'package:shopsmart_users_en/widgets/app_name_text.dart';
 
-import '../providers/theme_provider.dart';
-import '../widgets/title_text.dart';
+import '../services/assets_manager.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
-
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.shoppingCart),
+        ),
+        title: AppNameTextWidget(fontSize: 20),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SubtitleTextWidget(label: "Hello World!"),
-            TitlesTextWidget(label: "Hello this is a Title" * 10),
-            ElevatedButton(onPressed: () {}, child: Text("Hello World")),
-            SwitchListTile(
-              title: Text(
-                themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode",
+            SizedBox(height: 15),
+            SizedBox(
+              height: size.height * 0.25,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.asset(
+                      AppConstants.bannersImages[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  itemCount: AppConstants.bannersImages.length,
+                  pagination: SwiperPagination(
+                    // alignment: Alignment.center
+                    builder: DotSwiperPaginationBuilder(
+                      activeColor: Colors.red,
+                      color: Colors.white,
+                    ),
+                  ),
+                  // control: SwiperControl(),
+                ),
               ),
-              value: themeProvider.getIsDarkTheme,
-              onChanged: (value) {
-                themeProvider.setDarkTheme(themeValue: value);
-                // log("Theme state ${themeProvider.getIsDarkTheme}");
-              },
             ),
           ],
         ),
